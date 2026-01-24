634dc797f68edd541d794fcc65f3ba4b1281d733
richardsamillan <richiander24@gmail.com>
2026-01-24 01:55:42 -0500

    sistema subido

---
+++ src/main/java/EmpresaPkalab/PekalabApplication.java
@@ -0,0 +1,13 @@
+package EmpresaPkalab;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PekalabApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PekalabApplication.class, args);
+	}
+
+}
---
+++ src/test/java/EmpresaPkalab/PekalabApplicationTests.java
@@ -0,0 +1,13 @@
+package EmpresaPkalab;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PekalabApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
